53458052444a182423669bd76682d4faecdc3d68
ITVash <49442123+ITVash@users.noreply.github.com>
2025-08-12 23:50:35 +0300

    Sidebar added

--- shared/components/shared/main-sidebar.tsx
+++ shared/components/shared/main-sidebar.tsx
@@ -1,56 +1,99 @@
-import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "../ui/sidebar";
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
-import Image from 'next/image'
+"use client"
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+} from "../ui/sidebar"
+import { Settings, Folder, Search, LogOut, X, User } from "lucide-react"
+import Image from "next/image"
+import { Input } from "../ui/input"
+import React, { ChangeEvent } from "react"
+import { Button } from "../ui/button"
 
 export function AppSidebar() {
-  const items = [
-  {
-    title: "Home",
-    url: "#",
-    icon: Home,
-  },
-  {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
-  },
-  {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
-  },
-  {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
-  },
-]
-  return (
-    <Sidebar>
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel className="mx-[-8px] px-5 py-0 h-15 mb-2 border-b-2 rounded-none"><Image src={"./logo.svg"} alt='Logo' height={50} width={50} /> <span className="text-2xl pl-4">CashBazar</span></SidebarGroupLabel>
-          <SidebarGroupContent className="">
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-    </Sidebar>
-  )
+	const [search, setSearch] = React.useState<string>("")
+	const SearchHandler = (e: ChangeEvent<HTMLInputElement>) => {
+		setSearch(e.target.value)
+	}
+	const ClearSearchHandler = (): void => {
+		setSearch("")
+	}
+	const items = [
+		{
+			title: "Профиль",
+			url: "#",
+			icon: User,
+		},
+		{
+			title: "Административный раздел",
+			icon: Settings,
+			children: [
+				{
+					title: "Тест",
+					url: "#",
+				},
+			],
+		},
+		{
+			title: "Справочники",
+			url: "#",
+			icon: Folder,
+		},
+	]
+	return (
+		<Sidebar collapsible='offcanvas'>
+			<SidebarHeader className='flex items-center w-full flex-row border-b-2 rounded-none'>
+				<Image src={"./logo.svg"} alt='Logo' height={50} width={50} />{" "}
+				<span className='text-2xl pl-4'>CashBazar</span>
+			</SidebarHeader>
+			<SidebarContent>
+				<SidebarGroup>
+					<SidebarGroupContent className=''>
+						<SidebarMenu>
+							<SidebarMenuItem key={"Search input"}>
+								<SidebarMenuButton asChild>
+									<div className='relative'>
+										<Search className='absolute left-3' />{" "}
+										<Input
+											placeholder='Поиск'
+											className='h-7 pl-6 pr-6'
+											value={search}
+											onChange={SearchHandler}
+										/>
+										{search.length > 0 && (
+											<X
+												className='absolute right-3 cursor-pointer'
+												onClick={ClearSearchHandler}
+											/>
+										)}
+									</div>
+								</SidebarMenuButton>
+							</SidebarMenuItem>
+							{items.map((item) => (
+								<SidebarMenuItem key={item.title}>
+									<SidebarMenuButton asChild>
+										<a href={item.url}>
+											<item.icon />
+											<span>{item.title}</span>
+										</a>
+									</SidebarMenuButton>
+								</SidebarMenuItem>
+							))}
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+			</SidebarContent>
+			<SidebarFooter>
+				<Button>
+					<LogOut /> Выход
+				</Button>
+			</SidebarFooter>
+		</Sidebar>
+	)
 }
